313e51c36992806e94e9712a94edb1b23ce35778
Alexander Idelberger <alexander.idelberger@zalando.de>
2021-07-05 09:14:04 +0200

    Empty spring project

---
+++ src/main/java/com/example/logbookbomguavaissuedemo/LogbookBomGuavaIssueDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.logbookbomguavaissuedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbookBomGuavaIssueDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbookBomGuavaIssueDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/logbookbomguavaissuedemo/LogbookBomGuavaIssueDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logbookbomguavaissuedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbookBomGuavaIssueDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
